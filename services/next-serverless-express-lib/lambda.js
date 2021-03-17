86eb6d3b970507c3c6a68fa5bdb4f8e5a983eef9
stableford <stableford@stablefords-MacBook-Pro.local>
2021-03-18 09:09:56 +1100

    chore: :(

--- services/next-serverless-express-lib/lambda.js
+++ services/next-serverless-express-lib/lambda.js
@@ -1,21 +1,78 @@
-const next = require("next");
-const es = require("aws-serverless-express");
+// const next = require("next");
+// const es = require("aws-serverless-express");
 
-// const { failure } = require("../utils/browserResponse");
+// // const { failure } = require("../utils/browserResponse");
+
+// const app = next({ dev: false });
+
+// const handle = app.getRequestHandler();
+// const server = es.createServer(handle);
+
+// exports.handler = async function handler(event, context, callback) {
+//   try {
+//     const result = await es.proxy(server, event, context, "PROMISE").promise;
+//     return result;
+//   } catch (e) {
+//     // TODO: better way to handle errors
+//     console.error(e);
+//     callback(null, {});
+//     // callback(null, {await failure({ status: false }, e)});
+//   }
+// }
+
+// --------------
+
+const next = require("next");
+const { parse } = require("url");
+const serverlessExpress = require("@vendia/serverless-express");
 
 const app = next({ dev: false });
-
-const handle = app.getRequestHandler();
-const server = es.createServer(handle);
+const requestHandler = app.getRequestHandler();
 
 exports.handler = async function handler(event, context, callback) {
-  try {
-    const result = await es.proxy(server, event, context, "PROMISE").promise;
+  // try {
+  //   const result = await es.proxy(server, event, context, "PROMISE").promise;
+  //   return result;
+  // } catch (e) {
+  //   // TODO: better way to handle errors
+  //   console.error(e);
+  //   callback(null, {});
+  //   // callback(null, {await failure({ status: false }, e)});
+  // }
+
+  console.log({ event, context, callback });
+
+  // type RequestListener = (req: IncomingMessage, res: ServerResponse) => void;
+  const app = async (req, res) => {
+    // console.log({ req, res });
+
+    const parsedUrl = parse(req.url, true, false);
+
+    const result = await requestHandler(req, res, parsedUrl);
+
+    console.log(result);
+
     return result;
-  } catch (e) {
-    // TODO: better way to handle errors
-    console.error(e);
-    callback(null, {});
-    // callback(null, {await failure({ status: false }, e)});
-  }
-}
+  };
+
+  await serverlessExpress({ app })(event, context, callback);
+};
+
+// --------------
+
+// const next = require("next");
+// const { parse } = require("url");
+// const express = require("express");
+// const serverlessExpress = require("@vendia/serverless-express");
+
+// const nApp = next({ dev: false });
+// const handle = nApp.getRequestHandler();
+
+// const app = express();
+
+// app.get("/*", (req, res) => {
+//   const parsedUrl = new parse(req.url, true);
+//   handle(req, res, parsedUrl);
+// });
+
+// exports.handler = serverlessExpress({ app });
