907d59583a1a5c70d2e31c83be195efda3fb08d5
stableford <stableford@stablefords-MacBook-Pro.local>
2021-03-17 18:45:21 +1100

    chore: clean pages

--- services/demo-app/pages/index.js
+++ services/demo-app/pages/index.js
@@ -1,6 +1,12 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
+export async function getServerSideProps(context) {
+  return {
+    props: {}, // will be passed to the page component as props
+  }
+}
+
 export default function Home() {
   return (
     <div className={styles.container}>
