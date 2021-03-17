58b955177f888c3333baa094ea77ec480ee5fddc
stableford <stableford@stablefords-MacBook-Pro.local>
2021-03-17 20:28:50 +1100

    chore: sls config

---
+++ services/web-demo-app/handlers/index.js
@@ -0,0 +1,18 @@
+const serverless = require('serverless-http');
+const Koa = require('koa'); // or any supported framework
+
+const app = new Koa();
+
+app.use(/* register your middleware as normal */);
+
+// this is it!
+module.exports.handler = serverless(app);
+
+// or as a promise
+const handler = serverless(app);
+module.exports.handler = async (event, context) => {
+  // you can do other things here
+  const result = await handler(event, context);
+  // and here
+  return result;
+};
